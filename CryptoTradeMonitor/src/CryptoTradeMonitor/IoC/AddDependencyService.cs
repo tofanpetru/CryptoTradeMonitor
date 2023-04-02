1d86d32612c2df7cb153f7ca2175745909e2e99b
Tofan Petru <tofanpetru12@gmail.com>
2023-04-02 13:55:28 +0300

    add IoC

---
+++ CryptoTradeMonitor/src/CryptoTradeMonitor/IoC/AddDependencyService.cs
@@ -0,0 +1,17 @@
+﻿using Application.IoC;
+using Infrastructure.IoC;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CryptoTradeMonitor.IoC
+{
+    public static class AddDependencyService
+    {
+        public static IServiceCollection AddDependencyServiceExtension(this IServiceCollection services)
+        {
+            services.AddAppServiceExtension();
+            services.AddRepositoriesExtension();
+
+            return services;
+        }
+    }
+}
---
+++ CryptoTradeMonitor/src/Infrastructure/IoC/AddRepositories.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.IoC
+{
+    public static class AddRepositories
+    {
+        public static IServiceCollection AddRepositoriesExtension(this IServiceCollection appService)
+        {
+            var types = Assembly.GetExecutingAssembly().GetTypes();
+
+            types.Where(type => type.IsInterface).ToList()
+                .ForEach(interfac => types.Where(type => type.GetInterfaces().Contains(interfac)).ToList()
+                .ForEach(implementation => appService.AddScoped(interfac, implementation)));
+
+            return appService;
+        }
+    }
+}
