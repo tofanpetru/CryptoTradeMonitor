f90ca9eaddf6fcc0eacdecb9736a68fc0d4243dd
Tofan Petru <tofanpetru12@gmail.com>
2023-04-05 23:19:15 +0300

    add generic navigation

--- CryptoTradeMonitor/src/Application/Managers/ConsoleOutputManager.cs
+++ CryptoTradeMonitor/src/Application/Managers/ConsoleOutputManager.cs
@@ -13,6 +13,131 @@ namespace Application.Managers
             _exchangeManager = exchangeManager;
         }
 
+
+        public List<T> DisplayMenu<T>(List<T> items, int itemsPerPage = 5)
+        {
+            int currentPage = 0;
+            int totalPages = (int)Math.Ceiling(items.Count / (double)itemsPerPage);
+            List<int> selectedItemIndexes = new();
+            int currentSelectedItem = 0;
+            bool shouldClearConsole = true;
+
+            while (true)
+            {
+                if (shouldClearConsole)
+                {
+                    Console.Clear();
+                    Console.WriteLine($"Page {currentPage + 1}/{totalPages}");
+                    Console.WriteLine();
+
+                    for (int i = currentPage * itemsPerPage; i < (currentPage + 1) * itemsPerPage && i < items.Count; i++)
+                    {
+                        if (i == currentSelectedItem)
+                        {
+                            Console.BackgroundColor = ConsoleColor.White;
+                            Console.ForegroundColor = ConsoleColor.Black;
+                        }
+
+                        if (selectedItemIndexes.Contains(i))
+                        {
+                            Console.Write("* ");
+                        }
+                        else
+                        {
+                            Console.Write("  ");
+                        }
+
+                        Console.WriteLine($"{i + 1}. {items[i]}");
+
+                        if (i == currentSelectedItem)
+                        {
+                            Console.BackgroundColor = ConsoleColor.Black;
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                    }
+
+                    Console.WriteLine();
+                    Console.WriteLine("Use arrow keys to navigate, Space to select/deselect, and Enter to finish.");
+                }
+
+                shouldClearConsole = true;
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        if (currentSelectedItem > 0)
+                        {
+                            currentSelectedItem--;
+                        }
+                        else
+                        {
+                            shouldClearConsole = false;
+                        }
+                        if (currentSelectedItem < currentPage * itemsPerPage && currentPage > 0)
+                        {
+                            currentPage--;
+                            currentSelectedItem = (currentPage + 1) * itemsPerPage - 1;
+                        }
+                        break;
+
+                    case ConsoleKey.DownArrow:
+                        if (currentSelectedItem < items.Count - 1 && !(currentSelectedItem == items.Count - 1 && currentPage == totalPages - 1))
+                        {
+                            currentSelectedItem++;
+                        }
+                        else
+                        {
+                            shouldClearConsole = false;
+                        }
+                        if (currentSelectedItem >= (currentPage + 1) * itemsPerPage && currentPage < totalPages - 1)
+                        {
+                            currentPage++;
+                        }
+                        break;
+
+                    case ConsoleKey.LeftArrow:
+                        if (currentPage > 0)
+                        {
+                            currentPage--;
+                            currentSelectedItem = currentPage * itemsPerPage;
+                        }
+                        else
+                        {
+                            shouldClearConsole = false;
+                        }
+                        break;
+
+                    case ConsoleKey.RightArrow:
+                        if (currentPage < totalPages - 1)
+                        {
+                            currentPage++;
+                            currentSelectedItem = currentPage * itemsPerPage;
+                        }
+                        else
+                        {
+                            shouldClearConsole = false;
+                        }
+                        break;
+
+                    case ConsoleKey.Spacebar:
+                        if (selectedItemIndexes.Contains(currentSelectedItem))
+                        {
+                            selectedItemIndexes.Remove(currentSelectedItem);
+                        }
+                        else
+                        {
+                            selectedItemIndexes.Add(currentSelectedItem);
+                        }
+                        break;
+
+                    case ConsoleKey.Enter:
+                        return selectedItemIndexes.Select(i => items[i]).ToList();
+                }
+            }
+        }
+
+
         private async Task<List<string>> ChooseTradePairsAsync()
         {
             Console.WriteLine("Please enter the desired trade pairs, separated by a comma or space:");
