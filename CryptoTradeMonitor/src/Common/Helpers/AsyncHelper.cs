68dba8639a5010e887e7b34613ee453510536614
Tofan Petru <tofanpetru12@gmail.com>
2023-04-20 14:19:38 +0300

    add async helper for Run sync

---
+++ CryptoTradeMonitor/src/Common/Helpers/AsyncHelper.cs
@@ -0,0 +1,21 @@
+﻿namespace Common.Helpers
+{
+    public static class AsyncHelper
+    {
+        public static TResult RunSync<TResult>(Func<Task<TResult>> func)
+        {
+            if (SynchronizationContext.Current == null)
+            {
+                return Task.Run(func).GetAwaiter().GetResult();
+            }
+
+            TResult result = default;
+            var taskScheduler = TaskScheduler.FromCurrentSynchronizationContext();
+            var t = new Task(async () => { result = await func(); });
+            t.RunSynchronously(taskScheduler);
+            t.Wait();
+
+            return result;
+        }
+    }
+}
